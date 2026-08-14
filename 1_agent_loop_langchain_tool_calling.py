5bde579766e2751e0945499d5141d246fc2784a9
Victor <vemiliogp@gmail.com>
2026-08-14 22:42:59 +0200

    change model

--- 1_agent_loop_langchain_tool_calling.py
+++ 1_agent_loop_langchain_tool_calling.py
@@ -40,7 +40,7 @@ def run_agent(question: str):
     tools = [get_product_price, apply_discount]
     tools_dict = {t.name: t for t in tools}
 
-    llm = init_chat_model(f"openai:gpt-5", temperature=0)
+    llm = init_chat_model(f"ollama:{MODEL}", temperature=0)
     llm_with_tools = llm.bind_tools(tools)
 
     print(f"Question: {question}")
